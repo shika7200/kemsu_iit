54c8da4d724c097cb7b2d31fb1481ac021ed2204
Georgy Shive <shive1997me@gmail.com>
2024-06-22 14:10:56 +0300

    добавил раздел контакты в меню

--- src/widgets/MenuWidget/config/menuItems.ts
+++ src/widgets/MenuWidget/config/menuItems.ts
@@ -1,30 +1,33 @@
 export const menuItemsAuth: MenuItemProps[] = [
-
-    { text: 'Расписание', className: 'px-14 py-7', href: '/Schedule' },
-    { text: 'Сотрудники', className: 'px-12 py-6', href: '/Employees' },
-    { text: 'Новости', className: 'items-center px-16 py-7', href: "/News" },
-    { text: 'Методические материалы', className: 'px-7 pt-2', href: '/Materials' },
-    { text: 'Примеры заявлений', className: 'px-16 py-2 ', href: '/Examples' },
-    { text: 'Студенческая жизнь', className: 'px-9 py-2', href: '/StudentLife' },
-   
+  { text: "Расписание", className: "px-14 py-7", href: "/Schedule" },
+  { text: "Сотрудники", className: "px-12 py-6", href: "/Employees" },
+  { text: "Новости", className: "items-center px-16 py-7", href: "/News" },
+  {
+    text: "Методические материалы",
+    className: "px-7 pt-2",
+    href: "/Materials",
+  },
+  { text: "Примеры заявлений", className: "px-16 py-2 ", href: "/xamples" },
+  { text: "Студенческая жизнь", className: "px-9 py-2", href: "/StudentLife" },
+  { text: "Контакты", className: "items-center px-16 py-7", href: "/Сontacts" },
 ];
 
 export const menuItems: MenuItemProps[] = [
-    {
-        text: "Личный кабинет",
-        className: "px-2.5 py-7",
-        href: "/Login",
-    },
-    {
-        text: "Сотрудники",
-        className: "px-12 py-6",
-        href: "/Employees",
-    },
- 
-    {
-        text: "Новости",
-        className: "items-center px-16 py-7",
-        href: "/News",
-    },
-   
+  {
+    text: "Личный кабинет",
+    className: "px-2.5 py-7",
+    href: "/Login",
+  },
+  {
+    text: "Сотрудники",
+    className: "px-12 py-6",
+    href: "/Employees",
+  },
+
+  {
+    text: "Новости",
+    className: "items-center px-16 py-7",
+    href: "/News",
+  },
+  { text: "Контакты", className: "items-center px-16 py-7", href: "/Сontacts" },
 ];
