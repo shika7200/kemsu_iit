5c38aff4572eb898fdd6d7209122c2bf2ff1c633
Georgy Shive <shive1997me@gmail.com>
2024-06-25 00:56:13 +0300

    точно исправил

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -2,7 +2,7 @@
 import axios from 'axios';
 import Cookies from 'js-cookie';
 
- export const refreshToken = async (): Promise<string | null> => {
+ const refreshToken = async (): Promise<string | null> => {
   try {
     const response = await axios.post('/api/refresh-token');
     Cookies.set('access_token', response.data.token, { expires: 1 / 24, sameSite: 'Strict' }); // 1 hour
@@ -12,3 +12,4 @@ import Cookies from 'js-cookie';
     return null;
   }
 };
+ export default refreshToken;
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,4 +1,4 @@
 export {default as api} from './api';
-
+export {default as refreshToken} from './auth';
 export {default as isUserAuthenticated} from './isAuth';
 export {default as isHTML} from './isHTML';
