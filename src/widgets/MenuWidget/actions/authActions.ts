ef79fa71310082c40b17396dd62ae695a42d72ab
Georgy Shive <shive1997me@gmail.com>
2024-06-26 22:46:07 +0300

    явное удаление куки

--- src/widgets/MenuWidget/actions/authActions.ts
+++ src/widgets/MenuWidget/actions/authActions.ts
@@ -1,12 +1,18 @@
+const deleteCookie = (name: string, path: string, domain: string) => {
+  if (typeof window !== 'undefined') {
+    document.cookie = `${name}=; Max-Age=0; path=${path}; domain=${domain}; SameSite=None; Secure`;
+    console.log(`Cookie ${name} удалена`);
+  }
+};
+
 export const handleLogoutConfirm = (
   router: any, 
   setIsAuthenticated: (isAuthenticated: boolean) => void, 
   setShowLogoutConfirmation: (showLogoutConfirmation: boolean) => void
 ) => {
   if (typeof window !== 'undefined') {
-    // Удаляем токены из cookies и localStorage с учетом атрибута SameSite и Secure
-    document.cookie = 'access_token=; Max-Age=0; path=/; domain=' + window.location.hostname + '; SameSite=None; Secure';
-    console.log('Cookie access_token удалена');
+    // Удаляем токены из cookies и localStorage с учетом всех атрибутов
+    deleteCookie('access_token', '/', window.location.hostname);
     localStorage.removeItem('pocketbase_auth');
     console.log('localStorage pocketbase_auth удален');
 
