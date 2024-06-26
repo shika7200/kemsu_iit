0431ae49ea8deeadf928d3c6ce8bfb87c89834fb
Georgy Shive <shive1997me@gmail.com>
2024-06-26 22:35:58 +0300

    правки для попытки выйти из учетки на хосте

--- src/widgets/MenuWidget/actions/authActions.ts
+++ src/widgets/MenuWidget/actions/authActions.ts
@@ -1,14 +1,15 @@
 export const handleLogoutConfirm = (
-    router: any, 
-    setIsAuthenticated: (isAuthenticated: boolean) => void, 
-    setShowLogoutConfirmation: (showLogoutConfirmation: boolean) => void
-  ) => {
-    // Удаляем токены из cookies и localStorage
-    document.cookie = 'access_token=; Max-Age=0; path=/; domain=' + window.location.hostname;
-    localStorage.removeItem('pocketbase_auth');
-  
-    setIsAuthenticated(false);
-    setShowLogoutConfirmation(false);
-    router.push('/');
-  };
-  
+  router: any, 
+  setIsAuthenticated: (isAuthenticated: boolean) => void, 
+  setShowLogoutConfirmation: (showLogoutConfirmation: boolean) => void
+) => {
+  // Удаляем токены из cookies и localStorage с учетом атрибута SameSite и Secure
+  document.cookie = 'access_token=; Max-Age=0; path=/; domain=' + window.location.hostname + '; SameSite=None; Secure';
+  console.log('Cookie access_token удалена');
+  localStorage.removeItem('pocketbase_auth');
+  console.log('localStorage pocketbase_auth удален');
+
+  setIsAuthenticated(false);
+  setShowLogoutConfirmation(false);
+  router.push('/');
+};
